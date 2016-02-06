4266f6a87337b459e60e0675f7cd010b2c4435c1
kmuramat <kmuramat@yahoo-corp.jp>
2016-02-06 21:46:01 +0900

    node server を追加

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+
+app.get('/', function (req, res) {
+  var text = req.query.text;
+  text = encodeURIComponent(text);
+  var url = 'http://127.0.0.1:8080/summarize?sent_limit=3&text='+text;
+  request(url, function(error, response, body) {
+    if (error) return console.log(error);
+	  res.send(body);
+  });
+});
+
+app.listen(process.env.PORT || 3000);
